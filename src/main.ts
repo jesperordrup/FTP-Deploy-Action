fc986639b348a1d12a0ed10a68a7808b71d914a6
your username <jesper@umakers.dk>
2020-04-30 18:26:20 +0200

    building first time

--- src/main.ts
+++ src/main.ts
@@ -70,7 +70,7 @@ function withDefault(value: string, defaultValue: string) {
 async function syncFiles(args: IActionArguments) {
   try {
     await core.group("Uploading files", async () => {
-      return await exec.exec(`git ftp catchup --force --auto-init --verbose --syncroot ${args.local_dir} --user ${args.ftp_username} --passwd ${args.ftp_password} ${args.gitFtpArgs} ${args.ftp_server}`);
+      return await exec.exec(`git ftp ${args.gitFtpCommand} --force --auto-init --verbose --syncroot ${args.local_dir} --user ${args.ftp_username} --passwd ${args.ftp_password} ${args.gitFtpArgs} ${args.ftp_server}`);
     });
   }
   catch (error) {
